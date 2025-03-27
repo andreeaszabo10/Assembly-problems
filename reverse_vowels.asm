8f15ccc3266617061f57d0e5674e6ae6af123e8d
andreeaszabo10 <121065656+andreeaszabo10@users.noreply.github.com>
2025-03-27 22:59:32 +0200

    Update reverse_vowels.asm

--- reverse_vowels.asm
+++ reverse_vowels.asm
@@ -1,6 +1,5 @@
 section .data
-	; declare global vars here
-
+    	
 section .text
 	global reverse_vowels
     extern printf
@@ -12,16 +11,16 @@ reverse_vowels:
     pop ebp
     push ebx
     
-    ;; pun in edx adresa la care se afla string-ul
+    ;; push the address of the string into edx
     push dword [ebp + 8]
     pop edx
 
-;; loop pentru a pune pe stiva toate vocalele din string
+;; loop to push all vowels from the string onto the stack
 loop1:
-    ;; daca am ajuns la finalul string-ului trec la loop-ul in care adaug
+    ;; if we have reached the end of the string, go to the next loop to swap
     cmp byte [edx], 0
     je next
-    ;; verific caracterul si daca e vocala, sar la label-ul unde o pun pe stiva
+    ;; check the character, and if it is a vowel, jump to the label where we push it onto the stack
     cmp byte [edx], 'a'
     je next1
     cmp byte [edx], 'e'
@@ -32,29 +31,28 @@ loop1:
     je next1
     cmp byte [edx], 'u'
     je next1
-    ;; nu e vocala deci trec la urmatorul caracter pur si simplu
+    ;; it's not a vowel, so just move to the next character
     add edx, 1
     jmp loop1
     
 next1:
-    ;; pun vocala pe stiva apoi trec la urmatorul caracter
+    ;; push the vowel onto the stack and move to the next character
     push dword [edx]
     add edx, 1
     jmp loop1
 
-;; partea in care schimb vocalele cu cele din stiva
+;; part where we swap vowels with those on the stack
 next:
-    ;; pun iar in edx adresa string-ului ptr ca am modificat edx in loop-ul de sus
+    ;; push the string's address into edx again because we modified edx in the previous loop
     push dword [ebp + 8]
     pop edx
 
-;; verific daca fiecare caracter e vocala
+;; check if each character is a vowel
 loop2:
-    ;; verific daca am ajuns la finalul string-ului si daca da, ies
+    ;; check if we've reached the end of the string and if so, exit
     cmp byte [edx], 0
     je end
-    ;; verific daca e vocala caracterul la care ma aflu si daca e, sar
-    ;; la label-ul unde incepe schimbarea
+    ;; check if the current character is a vowel and jump to swap it
     cmp byte [edx], 'a'
     je next2
     cmp byte [edx], 'e'
@@ -65,15 +63,15 @@ loop2:
     je next2
     cmp byte [edx], 'u'
     je next2
-    ;; nu e vocala deci trec la urmatorul caracter
+    ;; it's not a vowel, so move to the next character
     add edx, 1
     jmp loop2
 
-;; scot din stiva ultima vocala pusa, si vad ce vocala e ca sa vad in ce caz ma duc
+;; pop the last vowel and check which one it is to know where to go
 next2:
-    ;; scot vocala
+    ;; pop the vowel
     pop ebx
-    ;; verific ce vocala e si merg in cazul corespunzator
+    ;; check which vowel it is and jump to the corresponding case
     cmp byte bl, 'a'
     je is_a
     cmp byte bl, 'e'
@@ -84,58 +82,53 @@ next2:
     je is_o
     jmp is_u
 
-;; daca litera pe care vreau sa o introduc e a
+;; if the vowel is 'a'
 is_a:
-    ;; fac and intre bitii literei si 0 ca sa ii resetez pe toti la 0
+    ;; reset all bits of the current character to 0
     and byte [edx], byte 0
-    ;; fac or intre bitii de la adresa literei pe care vreau sa o schimb
-    ;; (care acum sunt 0 )si bitii literei pe care vreau sa o adaug (adica a)
+    ;; set the bits of the current character to 'a'
     or byte [edx], 'a'
     jmp next3
 
-;; daca litera pe care vreau sa o introduc e e
+;; if the vowel is 'e'
 is_e:
-    ;; fac and intre bitii literei si 0 ca sa ii resetez pe toti la 0
+    ;; reset all bits of the current character to 0
     and byte [edx], byte 0
-    ;; fac or intre bitii de la adresa literei pe care vreau sa o schimb
-    ;; (care acum sunt 0) si bitii literei pe care vreau sa o adaug (adica e)
+    ;; set the bits of the current character to 'e'
     or byte [edx], 'e'
     jmp next3
 
-;; daca litera pe care vreau sa o introduc e i
+;; if the vowel is 'i'
 is_i:
-    ;; fac and intre bitii literei si 0 ca sa ii resetez pe toti la 0
+    ;; reset all bits of the current character to 0
     and byte [edx], byte 0
-    ;; fac or intre bitii de la adresa literei pe care vreau sa o schimb
-    ;; (care acum sunt 0) si bitii literei pe care vreau sa o adaug (adica i)
+    ;; set the bits of the current character to 'i'
     or byte [edx], 'i'
     jmp next3
 
-;; daca litera pe care vreau sa o introduc e o
+;; if the vowel is 'o'
 is_o:
-    ;; fac and intre bitii literei si 0 ca sa ii resetez pe toti la 0
+    ;; reset all bits of the current character to 0
     and byte [edx], byte 0
-    ;; fac or intre bitii de la adresa literei pe care vreau sa o schimb
-    ;; (care acum sunt 0) si bitii literei pe care vreau sa o adaug (adica o)
+    ;; set the bits of the current character to 'o'
     or byte [edx], 'o'
     jmp next3
 
-;; daca litera pe care vreau sa o introduc e u
+;; if the vowel is 'u'
 is_u:
-    ;; fac and intre bitii literei si 0 ca sa ii resetez pe toti la 0
+    ;; reset all bits of the current character to 0
     and byte [edx], byte 0
-    ;; fac or intre bitii de la adresa literei pe care vreau sa o schimb
-    ;; (care acum sunt 0) si bitii literei pe care vreau sa o adaug (adica u)
+    ;; set the bits of the current character to 'u'
     or byte [edx], 'u'
 
-;; sar aici cand am terminat de verificat caracterul curent
+;; jump here when done checking the current character
 next3:
-    ;; trec la pozitia urmatorului caracter
+    ;; move to the next character
     add edx, 1
-    ;; trec la urmatoarea verificare
+    ;; continue with the next check
     jmp loop2
 
-;; sar aici cand nu mai sunt alte caractere de verificat
+;; jump here when there are no more characters to check
 end:
     pop ebx
     push ebp
