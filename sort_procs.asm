6c8fda0f2ba886a8575dd4d8e7582bc955372b64
andreeaszabo10 <121065656+andreeaszabo10@users.noreply.github.com>
2025-03-27 23:05:46 +0200

    Update sort_procs.asm

--- sort_procs.asm
+++ sort_procs.asm
@@ -11,24 +11,21 @@ section .text
     extern printf
 
 sort_procs:
-    ;; DO NOT MODIFY
     enter 0,0
     pusha
 
     mov edx, [ebp + 8]      ; processes
     mov eax, [ebp + 12]     ; length
-    ;; DO NOT MODIFY
 
-    ;; Your code starts here
     mov ecx, 0 
-    ;; pun lungimea in ebx ca sa pot sa o modific
-    ;; fara sa pierd valoarea initiala
+    ;; store the length in ebx so I can modify it
+    ;; without losing the initial value
     mov ebx, eax
-    ;; fac contorul i sa fie 0
+    ;; set counter i to 0
     mov edi, 0
 
 outer:
-    ;; fac contorul j sa fie 0
+    ;; set counter j to 0
     mov esi, 0
     cmp edi, eax
     je end
@@ -36,25 +33,25 @@ outer:
     mov edi, 0
     sub ebx, 1
 
-;; verific fiecare proces cu toate restul
+;; check each process with all the others
 inner:
-        ;; verific daca trebuie sa trec la urmatorul proces
+        ;; check if I should move to the next process
         cmp esi, ebx
         jge newout
         push ebx
         mov ebx, 0
-        ;; edi creste cu 5 la fiecare iteratie ca sa pot sa verific
-        ;; procesul curent cu toate celelalte fara sa pierd adresa
+        ;; edi increments by 5 each iteration so I can check
+        ;; the current process with all the others without losing the address
         add edi, 5
         mov cl, byte [edx + proc.prio]
         mov bl, byte [edx + proc.prio + edi]
         cmp cl, bl
         jl no_swap
-        ;; daca prioritatile sunt egale, verific timpul, apoi pidul
+        ;; if priorities are equal, check time, then pid
         cmp cl, bl
         je time
 
-;; interschimbarea a 2 procese
+;; swapping two processes
 swap:
         mov cx, word [edx + proc.pid]
         mov bx, word [edx + proc.pid + edi]
@@ -69,23 +66,23 @@ swap:
         mov [edx + proc.time + edi], word cx
         mov [edx + proc.time], word bx
 
-;; nu interschimbam sau deja am interschimbat
+;; don't swap or already swapped
 no_swap:
-    ;; crestem contorul si reluam
+    ;; increment the counter and continue
     add esi, 1
     pop ebx
     jmp inner
 
 newout:
-    ;; trecem la urmatorul proces, crescand adresa cu 5
+    ;; move to the next process by increasing the address by 5
     lea edx, [edx + 5]
     pop edi
-    ;; crestem contorul
+    ;; increment the counter
     add edi, 1
     jmp outer
 
 time:
-    ;; daca timpul este mai mare interchimbam
+    ;; if the time is greater, swap
     mov cx, word [edx + proc.time]
     mov bx, word [edx + proc.time + edi]
     cmp cx, bx
@@ -94,7 +91,7 @@ time:
     jl no_swap
 
 pid:
-    ;; daca pidul este mai mare interchimbam
+    ;; if the pid is greater, swap
     mov cx, word [edx + proc.pid]
     mov bx, word [edx + proc.pid + edi]
     cmp cx, bx
@@ -102,10 +99,6 @@ pid:
     jmp no_swap
 
 end:
-    ;; Your code ends here
-    
-    ;; DO NOT MODIFY
     popa
     leave
     ret
-    ;; DO NOT MODIFY
