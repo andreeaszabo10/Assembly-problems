dfa14581be63bb446d4fe64ca5640335309e6648
andreeaszabo10 <121065656+andreeaszabo10@users.noreply.github.com>
2025-03-27 23:08:01 +0200

    Update pwd.asm

--- pwd.asm
+++ pwd.asm
@@ -1,86 +1,85 @@
 section .data
-	back db "..", 0
-	curr db ".", 0
-	slash db "/", 0
-	; declare global vars here
+    back db "..", 0
+    curr db ".", 0
+    slash db "/", 0
 
 section .text
-	global pwd
+    global pwd
     extern strcat
 
-;;	void pwd(char **directories, int n, char *output)
-;	Adauga in parametrul output path-ul rezultat din
-;	parcurgerea celor n foldere din directories
+;; void pwd(char **directories, int n, char *output)
+;; Adds the resulting path to the output parameter from
+;; traversing the n folders in directories
 pwd:
-	enter 0, 0
+    enter 0, 0
     pusha
    
-    mov edi, [ebp + 8] ;; vectorul de stringuri
-    mov ecx, [ebp + 12] ;; numarul de elemente
-    mov edx, [ebp + 16] ;; output-ul
+    mov edi, [ebp + 8] ;; array of strings
+    mov ecx, [ebp + 12] ;; number of elements
+    mov edx, [ebp + 16] ;; output
 
-    ;; am pus slash-ul de inceput in output
+    ;; add the leading slash to the output
     mov [edx], byte '/'
-    ;; am initializat contorul cu nr de directoare ca sa scad din el
+    ;; initialize the counter with the number of directories to subtract from it
     mov esi, ecx
 
 new_directory:
-    ;; daca am terminat de citit directoare ies
+    ;; if we've finished reading directories, exit
     cmp esi, 0
     je end
-    ;; modific contorul
+    ;; decrement the counter
     sub esi, 1
-    ;; pun in ebx adresa directorului curent
+    ;; store the address of the current directory in ebx
     xor ebx, ebx
     mov ebx, dword [edi]
-    ;; daca directorul curent este . sau .. nu scriu
+    ;; if the current directory is . or .., don't write it
     cmp byte [ebx], '.'
     je dont_write
-    ;; pun in eax adresa urmatorului director pentru ca daca urmatorul
-    ;; este .., nu il mai pun in output pe cel curent
+    ;; store the address of the next directory in eax
+    ;; because if the next directory is .., we shouldn't add the current one to the output
     xor eax, eax
     mov eax, dword [edi + 4]
-    ;; daca incepe cu . poate fi .. deci verific daca scriu sau nu
+    ;; if it starts with ., it could be .., so we need to check if we write it or not
     cmp byte [eax], '.'
     je verify
 
-;; scriu in output directorul curent
+;; write the current directory to the output
 write:
-    ;; concatenez directorul la ce era deja in output
+    ;; concatenate the directory to what was already in the output
     push ebx
     push edx
     call strcat
     add esp, 8
-    ;; ma asigur ca edx are adresa buna
+    ;; ensure that edx has the correct address
     mov edx, [ebp + 16]
-    ;; trec la urmatorul director din vector
+    ;; move to the next directory in the array
     add edi, 4
-    ;; concatenez si un slash
+    ;; concatenate a slash
     push slash
     push edx
     call strcat
     add esp, 8
-    ;; ma asigur ca edx are adresa buna
+    ;; ensure that edx has the correct address
     mov edx, [ebp + 16]
     jmp new_directory
 
-;; trec la adresa urmatorului director si nu il scriu pe cel curent
+;; move to the next directory address and don't write the current one
 dont_write: 
     add edi, 4
     jmp new_directory
 
-;; verific daca .. este pe pozitia urmatoare sau a doua care urmeaza
+;; check if .. is in the next position or two positions ahead
 verify:
-    ;; urmeaza .. deci nu scriu in output directorul curent
+    ;; if .. follows, don't write the current directory in the output
     cmp byte [eax + 1], '.'
     je dont_write
-    ;; verifc daca .. e pe pozitia a 2 a urmatoare
+    ;; check if .. is in the second next position
     xor eax, eax
     mov eax, dword [edi + 8]
-    ;; daca nu incepe cu . n-are cum sa fie .. 
+    ;; if it doesn't start with ., it can't be ..
     cmp byte [eax], '.'
     jne write
-    ;; daca e .. nu scriu, altfel e doar . deci scriu
+    ;; if it is .., don't write it, otherwise, if it's just ., write it
     cmp byte [eax + 1], '.'
     je dont_write
     jne write
